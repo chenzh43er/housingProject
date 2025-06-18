4c123fa4c0983232e5858144f3716ff41d829dd6
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-06-19 03:06:02 +0800

    update lang

--- de/Public/Js/common.js
+++ de/Public/Js/common.js
@@ -2,3 +2,7 @@ function getLangFromPath() {
     const pathSegments = window.location.pathname.split('/');
     return pathSegments[1]; // 假设语言代码总是在第一个路径段
 }
+
+function returnAdvWord(){
+    return `<p style="text-align: center">Advertisment</p>`
+}
