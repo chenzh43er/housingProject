f4dc4e5a66671a9dae4916de80dc763a8c5251db
chenzh43er <chenzh43er@DESKTOP-AOQJSMC>
2025-06-24 01:13:09 +0800

    update pic

--- de/Public/Js/common.js
+++ de/Public/Js/common.js
@@ -6,3 +6,11 @@ function getLangFromPath() {
 function returnAdvWord(){
     return `<p style="text-align: center;text-align: center; line-height: 100px; margin: 0;width: 100%">Advertisment</p>`
 }
+
+function returnAdvTestWord(){
+    return `<p style="text-align: center;text-align: center; line-height: 100px; margin: 0;width: 100%">ADV Test</p>`
+}
+
+function isMobile() {
+    return window.matchMedia("only screen and (max-width: 768px)").matches;
+}
